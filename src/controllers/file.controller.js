d87a54988ecab22a1feb6471be374dbf7a250a19
aadiix23 <aadiix23@gmail.com>
2025-12-25 03:49:42 +0530

    refactor ->added the View Full Screen API

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -2,6 +2,8 @@ const { getClient } = require("../config/telegram");
 const File = require("../models/File")
 const { Api } = require("telegram");
 const fs = require("fs");
+const { error } = require("console");
+const { message } = require("telegram/client");
 
 exports.uploadFile = async (req, res) => {
     const client = getClient();
@@ -48,6 +50,32 @@ exports.getFiles=async (req,res)=>{
     } catch (error) {
         console.error(err);
         res.status(500).json({error:err.message});
+        
+    }
+};
+exports.viewFile=async(req,res)=>{
+    try {
+        const client=getClient();
+        const fileId=req.params.id;
+        const file=await File.findById(fileId);
+        if(!file){
+            return res.status(404).json({error:"File Not Found"})
+        }
+        const messages=await client.getMessages("me",{
+            ids:file.telegramMessageId,
+        });
+        const message=messages[0];
+        if(!message||!message.media){
+            return res.status(404).json({error:"Media Not Found In Database"})
+        }
+        const buffer=await client.downloadMedia(message.media,{
+            workers:1,
+        });
+        res.setHeader("Content-Type",file.mimeType);
+        res.send(buffer);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error:err.message})
         
     }
 };
